9279ad728dd95ca45d3dbcc59d4f1abed9df9efb
Kilavagora <kilavagora@users.noreply.github.com>
2019-07-28 17:58:16 -0500

    Minor fixes. Update version

--- app.js
+++ app.js
@@ -1,133 +1,145 @@
-let express = require("express");
-let bodyParser = require("body-parser");
-let mongodb = require("mongodb");
-let ObjectID = mongodb.ObjectID;
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
+const ObjectID = mongodb.ObjectID;
 
 const NOTES_COLLECTION = "notes";
 
-let app = express();
+const app = express();
 app.use(bodyParser.json());
 
 // Static file server
-let staticDir = __dirname + "/static/";
+const staticDir = __dirname + "/static/";
 app.use(express.static(staticDir));
 
 let db;
 
 // Connect to the database and if successful start the server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://mongo:27017/notes_db")
-  .then((database) => {
+  .then(database => {
     db = database;
-    console.log("Database connection ready");
-    let server = app.listen(process.env.PORT || 8080, () => {
-      let port = server.address().port;
-      console.log("App now running on port", port);
+    console.info("Database connection ready");
+    const server = app.listen(process.env.PORT || 8080, () => {
+      const port = server.address().port;
+      console.info("App now running on port", port);
     });
-  }).catch((err) => {
-    console.log(err);
+  })
+  .catch(err => {
+    console.error(err);
     process.exit(1);
   });
 
 // Error handler.
 function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
+  console.error("ERROR: " + reason);
   res.status(code || 500).json({
-    "error": message
+    error: message
   });
 }
 
 // GET: list all notes
-app.get("/api/notes", (req, res) => {
-  db.collection(NOTES_COLLECTION).find({}).toArray()
-    .then((docs) => {
+app.get("/api/notes", (_, res) => {
+  db.collection(NOTES_COLLECTION)
+    .find({})
+    .toArray()
+    .then(docs => {
       res.status(200).json(docs);
     })
-    .catch((err) => {
+    .catch(err => {
       handleError(res, err.message, "Failed to get notes.");
     });
 });
 
 // POST: create a new note
 app.post("/api/notes", (req, res) => {
-  let newnote = req.body;
+  const newnote = req.body;
   newnote.createDate = new Date();
 
   if (!req.body.note) {
     handleError(res, "Invalid user input", "Must provide a note.", 400);
+    return;
   }
 
-  db.collection(NOTES_COLLECTION).insertOne(newnote)
-    .then((doc) => {
+  db.collection(NOTES_COLLECTION)
+    .insertOne(newnote)
+    .then(doc => {
       res.status(201).json(doc.ops[0]);
     })
-    .catch((err) => {
+    .catch(err => {
       handleError(res, err.message, "Failed to create new note.");
     });
-
 });
 
 // GET: find note by id
 app.get("/api/notes/:id", (req, res) => {
-
-  let id = req.params.id;
+  const id = req.params.id;
   if (!ObjectID.isValid(id)) {
     handleError(res, "Invalid id", "Must provide a valid id.", 400);
+    return;
   }
 
-  db.collection(NOTES_COLLECTION).findOne({
+  db.collection(NOTES_COLLECTION)
+    .findOne({
       _id: new ObjectID(id)
     })
-    .then((doc) => {
+    .then(doc => {
       if (!doc) {
         handleError(res, "Id does not exist", "Not found :(", 404);
         return;
       }
       res.status(200).json(doc);
     })
-    .catch((err) => {
+    .catch(err => {
       handleError(res, err.message, "Failed to get note");
     });
 });
 
 // PUT: update note by id
 app.put("/api/notes/:id", (req, res) => {
-
   let id = req.params.id;
   if (!ObjectID.isValid(id)) {
     handleError(res, "Invalid id", "Must provide a valid id.", 400);
+    return;
   }
 
-  let updateDoc = req.body;
+  const updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(NOTES_COLLECTION).updateOne({
-      _id: new ObjectID(id)
-    }, updateDoc)
-    .then((doc) => {
+  db.collection(NOTES_COLLECTION)
+    .updateOne(
+      {
+        _id: new ObjectID(id)
+      },
+      updateDoc
+    )
+    .then(() => {
       updateDoc._id = req.params.id;
       res.status(200).json(updateDoc);
     })
-    .catch((err) => {
+    .catch(err => {
       handleError(res, err.message, "Failed to update note");
     });
 });
 
 // DELETE: deletes note by id
 app.delete("/api/notes/:id", (req, res) => {
-
-  let id = req.params.id;
+  const id = req.params.id;
   if (!ObjectID.isValid(id)) {
     handleError(res, "Invalid id", "Must provide a valid id.", 400);
+    return;
   }
 
-  db.collection(NOTES_COLLECTION).deleteOne({
-    _id: new ObjectID(id)
-  }).then((result) => {
-    res.status(200).json({
-      id: req.params.id,
-      result: result
+  db.collection(NOTES_COLLECTION)
+    .deleteOne({
+      _id: new ObjectID(id)
+    })
+    .then(result => {
+      res.status(200).json({
+        id: req.params.id,
+        result: result
+      });
+    })
+    .catch(err => {
+      handleError(res, err.message, "Failed to delete note");
     });
-  }).catch((err) => {
-    handleError(res, err.message, "Failed to delete note");
-  });
 });
